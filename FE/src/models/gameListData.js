05596e8aff59dd000c4e9695e97ddd22c57de263
sohyun4283 <sohyun4283@naver.com>
2020-05-06 19:31:34 +0900

    [#11] feat: 메인화면 팀 리스트 mock data 생성
    - 일단 프론트 환경에 맞춘 mock data 추가
    - 추후 서버에서 제공하는 data로 교체 예쩡

---
+++ FE/src/models/gameListData.js
@@ -0,0 +1,28 @@
+const gameListData = [
+  {
+    matchName: "GAME 1",
+    awayTeam: "SAMSUNG",
+    homeTeam: "LOTTE",
+    isAvailable: true,
+  },
+  {
+    matchName: "GAME 2",
+    awayTeam: "Captin",
+    homeTeam: "Marvel",
+    isAvailable: true,
+  },
+  {
+    matchName: "GAME 3",
+    awayTeam: "Twins",
+    homeTeam: "Tigers",
+    isAvailable: false,
+  },
+  {
+    matchName: "GAME 4",
+    awayTeam: "Rockets",
+    homeTeam: "Dodgers",
+    isAvailable: true,
+  },
+];
+
+export default gameListData;
