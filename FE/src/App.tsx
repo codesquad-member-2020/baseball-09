f1eea24edd50c038ff822d19b384e1b180de5d51
sangwon21 <junothyme@gmail.com>
2020-05-06 21:40:14 +0900

    [#16] feat: 라우팅 설정 및 에러 페이지 작성
    why: SPA에 필요한 라우팅 설정을 했다. 더불어 사용자가 제공하지 않는
    경로를 접근할 경우 보여줄, 에러 페이지를 작성했다.

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import MainPage from "@/components/MainPage/MainPage";
+import ErrorPage from "@/components/ErrorPage/ErrorPage";
 
 const App = () => {
   return (
     <>
-      <MainPage />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={MainPage} />
+          <Route component={ErrorPage} />
+        </Switch>
+      </BrowserRouter>
     </>
   );
 };
