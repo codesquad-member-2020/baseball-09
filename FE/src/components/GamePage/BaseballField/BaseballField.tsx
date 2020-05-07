aeb051f105e6e562721a1ff050bba219095d4bba
sohyun4283 <sohyun4283@naver.com>
2020-05-08 08:54:24 +0900

    [#29] refactor: 필드 영역 컴포넌트 분리
    - 애니메이션과 이벤트를 적용하기 위해 컴포넌트 분리함

--- FE/src/components/GamePage/BaseballField/BaseballField.tsx
+++ FE/src/components/GamePage/BaseballField/BaseballField.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import CurrentInfo from "@/components/GamePage/BaseballField/CurrentInfo/CurrentInfo";
-import PitcherImg from "./fitcher.png";
-import HitterImg from "./hitter.png";
+import Field from "@/components/GamePage/BaseballField/Field/Field";
 import FieldImg from "./field.png";
 
 const Wrapper = styled.div`
@@ -13,61 +12,13 @@ const Wrapper = styled.div`
   -webkit-perspective: 800px;
   transform-style: preserve-3d;
   transition: transform 1s ease-in;
-  .field {
-    position: relative;
-    height: inherit;
-    background: url(${FieldImg}) no-repeat center;
-    background-size: contain;
-  }
 `;
 
-const Playball = styled.button`
-  position: absolute;
-  color: white;
-  font-family: sans-serif;
-  font-size: 1.5em;
-  cursor: pointer;
-  background-color: #4682b4;
-  border-radius: 5%;
-  border: solid 1px #75b6ed;
-  padding: 0.25em 1em;
-  right: 43%;
-  top: 47%;
-  &:hover {
-    background-color: #00008b;
-  }
-`;
-
-const Pitcher = styled.img`
-  height: 50px;
-  position: absolute;
-  right: 48%;
-  top: 56%;
-`;
-
-const Hit = keyframes`
-  100%{  
-    transform: translate(230px, -240px);
-  }
-`;
-
-const HitterWrap = styled.div`
-  position: absolute;
-  width: 50px;
-  height: 50px;
-  right: 48%;
-  top: 86.5%;
-  animation: ${Hit} 3s 1.2s linear 1;
-  animation-fill-mode: forwards;
-`;
-
-const Hitter = styled.div`
-  position: absolute;
-  height: 100%;
-  z-index: 100;
-  .hitter-img {
-    height: inherit;
-  }
+const FieldWrap = styled.div`
+  position: relative;
+  height: inherit;
+  background: url(${FieldImg}) no-repeat center;
+  background-size: contain;
 `;
 
 const BaseballField = () => {
@@ -75,15 +26,9 @@ const BaseballField = () => {
     <>
       <Wrapper>
         <CurrentInfo />
-        <div className="field">
-          <Playball>PITCH!</Playball>
-          <Pitcher src={PitcherImg} />
-          <HitterWrap>
-            <Hitter>
-              <img className="hitter-img" src={HitterImg} />
-            </Hitter>
-          </HitterWrap>
-        </div>
+        <FieldWrap>
+          <Field />
+        </FieldWrap>
       </Wrapper>
     </>
   );
