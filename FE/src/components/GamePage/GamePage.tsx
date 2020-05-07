823228901173a38edd58c7b4d878ff44200cf780
sangwon21 <junothyme@gmail.com>
2020-05-07 15:53:30 +0900

    [#25] chore: vscode에서 alias을 인식 못하는 오류 해결
    why: vscode intellisense에서 alias를 인식하지 못해서, 계속해서 빨간
    오류가 떴다. tsconfig.json에서 alias를 인식할 수 있도록 옵션을 부여했다.

---
+++ FE/src/components/GamePage/GamePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const HeaderSection = styled.section`
+  border-bottom: 1px solid black;
+  height: 20vh;
+  width: 100vw;
+`;
+
+const GamePage = () => {
+  return <HeaderSection></HeaderSection>;
+};
+
+export default GamePage;
