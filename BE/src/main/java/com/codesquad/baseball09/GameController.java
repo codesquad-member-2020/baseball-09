6c7dfa221db84a7b8733e95d10fce8125b47dcb2
Han <quaint3@gmail.com>
2020-05-07 15:43:16 +0900

    [#22] feature : /game
    get mapping, mock데이터 하드코딩 되어있음

--- BE/src/main/java/com/codesquad/baseball09/GameController.java
+++ BE/src/main/java/com/codesquad/baseball09/GameController.java
@@ -1,6 +1,15 @@
 package com.codesquad.baseball09;
 
+import static com.codesquad.baseball09.model.State.BALL;
+import static com.codesquad.baseball09.model.State.OUT;
+import static com.codesquad.baseball09.model.State.STRIKE;
+
+import com.codesquad.baseball09.model.BatterLog;
+import com.codesquad.baseball09.model.Board;
+import com.codesquad.baseball09.model.Game;
 import com.codesquad.baseball09.model.Match;
+import com.codesquad.baseball09.model.State;
+import com.codesquad.baseball09.model.Team;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -19,4 +28,53 @@ public class GameController {
         new Match("Lotte", "NC")
     ).collect(Collectors.toList());
   }
+
+  @GetMapping("/game")
+  public Board getBoard() {
+    BatterLog l1 = new BatterLog(
+        "추신수",
+        5
+    );
+    l1.addLogs(BALL);
+    l1.addLogs(STRIKE);
+    l1.addLogs(STRIKE);
+    l1.addLogs(STRIKE);
+    l1.addLogs(OUT);
+
+    BatterLog l2 = new BatterLog(
+        "이용대",
+        6
+    );
+    l2.addLogs(STRIKE);
+    l2.addLogs(BALL);
+    l2.addLogs(BALL);
+    l2.addLogs(State.HIT);
+
+    BatterLog l3 = new BatterLog(
+        "류현진",
+        7
+    );
+    l3.addLogs(STRIKE);
+    l3.addLogs(BALL);
+    l3.addLogs(BALL);
+    l3.addLogs(BALL);
+    l3.addLogs(STRIKE);
+
+    Board board = new Board(
+        new Team(1L, "Captin", 1),
+        new Team(2L, "Marvel", 5),
+        2,
+        false,
+        2,
+        3,
+        2,
+        new Game("최동원", "류현진")
+    );
+    board.addBatterLog(l1);
+    board.addBatterLog(l2);
+    board.addBatterLog(l3);
+
+    return board;
+  }
+
 }
