38507b22d6626c3402dbb310e7cf8cade912612c
sohyun4283 <sohyun4283@naver.com>
2020-05-08 08:24:34 +0900

    [#29] feat: 현재 필드 정보 영역 추가
    - 정보 보여주는 영역을 추가함

--- FE/src/components/GamePage/BaseballField/BaseballField.tsx
+++ FE/src/components/GamePage/BaseballField/BaseballField.tsx
@@ -20,6 +20,32 @@ const Wrapper = styled.div`
   }
 `;
 
+const Header = styled.div`
+  position: absolute;
+  height: 20%;
+  width: 100%;
+  z-index: 100;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  padding: 5px 20px;
+  font-size: 24px;
+  font-weight: 600;
+  color: #fff;
+  .status-wrap {
+    flex-direction: column;
+  }
+  .status {
+    display: flex;
+    height: 40px;
+    align-items: center;
+  }
+  .title {
+    width: 30px;
+    margin: 0;
+  }
+`;
+
 const Playball = styled.button`
   position: absolute;
   color: white;
@@ -73,6 +99,27 @@ const BaseballField = () => {
   return (
     <>
       <Wrapper>
+        <Header>
+          <div className="status-wrap">
+            <div className="status">
+              <p className="title">S</p>
+              <span>●</span>
+              <span>●</span>
+              <span>●</span>
+            </div>
+            <div className="status">
+              <p className="title">B</p>
+              <span>●</span>
+              <span>●</span>
+            </div>
+            <div className="status">
+              <p className="title">O</p>
+              <span>●</span>
+              <span>●</span>
+            </div>
+          </div>
+          <p>2회초 수비</p>
+        </Header>
         <div className="field">
           <Playball>PITCH!</Playball>
           <PitcherImg src={Pitcher} />
