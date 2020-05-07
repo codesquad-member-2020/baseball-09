8594cbb8604c52a9e1468ff5e27c6d0ad42ad1fc
sohyun4283 <sohyun4283@naver.com>
2020-05-08 08:41:11 +0900

    [#29] refactor: 필드 현재 정보 부분 컴포넌트 분리
    - 컴포넌트 단위가 커 분리함

--- FE/src/components/GamePage/BaseballField/BaseballField.tsx
+++ FE/src/components/GamePage/BaseballField/BaseballField.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
+import CurrentInfo from "@/components/GamePage/BaseballField/CurrentInfo/CurrentInfo";
 import Pitcher from "./fitcher.png";
 import HitterImg from "./hitter.png";
 import Field from "./field.png";
@@ -17,32 +18,6 @@ const Wrapper = styled.div`
     height: inherit;
     background: url(${Field}) no-repeat center;
     background-size: contain;
-  }
-`;
-
-const Header = styled.div`
-  position: absolute;
-  height: 20%;
-  width: 100%;
-  z-index: 100;
-  display: flex;
-  align-items: flex-start;
-  justify-content: space-between;
-  padding: 5px 20px;
-  font-size: 24px;
-  font-weight: 600;
-  color: #fff;
-  .status-wrap {
-    flex-direction: column;
-  }
-  .status {
-    display: flex;
-    height: 40px;
-    align-items: center;
-  }
-  .title {
-    width: 30px;
-    margin: 0;
   }
 `;
 
@@ -99,27 +74,7 @@ const BaseballField = () => {
   return (
     <>
       <Wrapper>
-        <Header>
-          <div className="status-wrap">
-            <div className="status">
-              <p className="title">S</p>
-              <span>●</span>
-              <span>●</span>
-              <span>●</span>
-            </div>
-            <div className="status">
-              <p className="title">B</p>
-              <span>●</span>
-              <span>●</span>
-            </div>
-            <div className="status">
-              <p className="title">O</p>
-              <span>●</span>
-              <span>●</span>
-            </div>
-          </div>
-          <p>2회초 수비</p>
-        </Header>
+        <CurrentInfo />
         <div className="field">
           <Playball>PITCH!</Playball>
           <PitcherImg src={Pitcher} />
