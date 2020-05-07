be723db9c0184d07d2b1fb53ce3beb634b315448
Han <quaint3@gmail.com>
2020-05-07 14:56:06 +0900

    [#22] feature : /main
    팀 선택창에 대한 mock 데이터 배포

--- BE/src/main/java/com/codesquad/baseball09/GameController.java
+++ BE/src/main/java/com/codesquad/baseball09/GameController.java
@@ -1,8 +1,22 @@
 package com.codesquad.baseball09;
 
+import com.codesquad.baseball09.model.Match;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GameController {
 
+  @GetMapping("/main")
+  public List<Match> main() {
+    return Stream.of(
+        new Match("Captin", "Marvel"),
+        new Match("Twins", "Tigers"),
+        new Match("Rockets", "Doggers"),
+        new Match("Lotte", "NC")
+    ).collect(Collectors.toList());
+  }
 }
