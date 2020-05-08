5662143fa5c4de4569eed5ea9861fe97e976035c
Han <quaint3@gmail.com>
2020-05-08 15:33:45 +0900

    [#36] feature : findAll() 메서드 추가
    /main 에 해당하는 페이지 데이터를 주기 위하여

--- BE/src/main/java/com/codesquad/baseball09/repository/GameRepository.java
+++ BE/src/main/java/com/codesquad/baseball09/repository/GameRepository.java
@@ -1,5 +1,9 @@
 package com.codesquad.baseball09.repository;
 
+import com.codesquad.baseball09.model.Match;
+import java.util.List;
+
 public interface GameRepository {
 
+  List<Match> findAll();
 }
--- BE/src/main/java/com/codesquad/baseball09/repository/JdbcGameRepository.java
+++ BE/src/main/java/com/codesquad/baseball09/repository/JdbcGameRepository.java
@@ -1,8 +1,36 @@
 package com.codesquad.baseball09.repository;
 
+import com.codesquad.baseball09.model.Match;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class JdbcGameRepository {
+public class JdbcGameRepository implements GameRepository {
+
+  private final Logger log = LoggerFactory.getLogger(JdbcGameRepository.class);
+  private final JdbcTemplate jdbcTemplate;
+
+  public JdbcGameRepository(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  @Override
+  public List<Match> findAll() {
+    return jdbcTemplate.query(
+        "SELECT m.id, h.name as home, a.name as away, m.selected "
+            + "FROM MATCH m "
+            + "LEFT JOIN TEAM h ON m.home_team_id = h.id "
+            + "LEFT JOIN TEAM a ON m.away_team_id = a.id"
+        , new Object[]{}, (rs, rowNum) -> new Match(
+            rs.getLong("id"),
+            rs.getString("home"),
+            rs.getString("away"),
+            rs.getBoolean("selected")
+        ));
+  }
+
 
 }
