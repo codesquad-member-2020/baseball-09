5ab560543bd4145d703412a4283a53ead5cccd90
Han <quaint3@gmail.com>
2020-05-08 15:31:36 +0900

    [#36] feature : match selected 변수 추가
    해당 게임이 선택되었는 지 알기 위한 변수 추가

--- BE/src/main/java/com/codesquad/baseball09/model/Match.java
+++ BE/src/main/java/com/codesquad/baseball09/model/Match.java
@@ -1,13 +1,24 @@
 package com.codesquad.baseball09.model;
 
+import org.springframework.data.annotation.Id;
+
 public class Match {
 
+  @Id
+  private final Long id;
   private final String home;
   private final String away;
+  private final boolean selected;
 
-  public Match(String home, String away) {
+  public Match(Long id, String home, String away, boolean selected) {
+    this.id = id;
     this.home = home;
     this.away = away;
+    this.selected = selected;
+  }
+
+  public Long getId() {
+    return id;
   }
 
   public String getHome() {
@@ -16,5 +27,9 @@ public class Match {
 
   public String getAway() {
     return away;
+  }
+
+  public boolean isSelected() {
+    return selected;
   }
 }
