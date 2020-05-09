ad0695182b25a9399f3d6792cfb6e8c830592923
limwin94 <amuguna94@gmail.com>
2020-05-09 17:05:00 +0900

    [#37] feat: 커스텀 셀 파일 생성 및 Label 연결

---
+++ iOS/BaseBallApp/BaseBallApp/View/CurrentPlayerCell.swift
@@ -0,0 +1,27 @@
+//
+//  CurrentPlayerCell.swift
+//  BaseBallApp
+//
+//  Created by 임승혁 on 2020/05/09.
+//  Copyright © 2020 임승혁. All rights reserved.
+//
+
+import UIKit
+
+class CurrentPlayerCell: UITableViewCell {
+    @IBOutlet weak var playerType: UILabel!
+    @IBOutlet weak var playerName: UILabel!
+    @IBOutlet weak var playerInfo: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
