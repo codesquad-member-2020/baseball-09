35a2635b5dfce5f769e0aaa30d729ae938e6f8f8
Han <quaint3@gmail.com>
2020-05-11 19:44:16 +0900

    [#46] feature : game 시작 요청 후, 완료되었다는 클래스
    게임 시작 요청을 보내면, 게임이 시작되었다는 응답 클래스 생성

---
+++ BE/src/main/java/com/codesquad/baseball09/model/api/response/GameResponse.java
@@ -0,0 +1,14 @@
+package com.codesquad.baseball09.model.api.response;
+
+public class GameResponse {
+
+  private Long id;
+
+  public GameResponse(Long id) {
+    this.id = id;
+  }
+
+  public Long getId() {
+    return id;
+  }
+}
