094b6a957b03d63b2b3c10af7bada8c7c36c769c
Han <quaint3@gmail.com>
2020-05-11 13:05:39 +0900

    [#36] feature : 팀 선택 request 객체 생성
    팀 선택 request 객체 생성

---
+++ BE/src/main/java/com/codesquad/baseball09/model/api/request/TeamRequest.java
@@ -0,0 +1,15 @@
+package com.codesquad.baseball09.model.api.request;
+
+public class TeamRequest {
+
+  private Long id;
+  private Boolean isSelected;
+
+  public Long getId() {
+    return id;
+  }
+
+  public Boolean getIsSelected() {
+    return isSelected;
+  }
+}
