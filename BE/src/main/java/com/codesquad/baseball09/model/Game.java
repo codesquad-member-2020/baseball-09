52a0bda5f08e7259e4f3cc8530f05e80efe12c5c
Han <quaint3@gmail.com>
2020-05-12 12:03:54 +0900

    [#46] feature : 대결중인 플레이어들을 관리할 객체 수정
    이 객체는, home, away player들의 리스트를 기억하고 있어야하고,
    진행에 따라 vs하고 있는 player들이 변경되어야함.

--- BE/src/main/java/com/codesquad/baseball09/model/Game.java
+++ BE/src/main/java/com/codesquad/baseball09/model/Game.java
@@ -1,22 +1,65 @@
 package com.codesquad.baseball09.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
-  private String pitcher;
+  private Player home;
 
-  private String batter;
+  private Player away;
 
-  public Game(String pitcher, String batter) {
-    this.pitcher = pitcher;
-    this.batter = batter;
+  @JsonIgnore
+  private List<Player> homePlayers = new ArrayList<>();
+  @JsonIgnore
+  private List<Player> awayPlayers = new ArrayList<>();
+
+  @JsonIgnore
+  private int homeOrder;
+  @JsonIgnore
+  private int awayOrder;
+
+  private Game(Player home, Player away,
+      List<Player> homePlayers, List<Player> awayPlayers) {
+    this.home = home;
+    this.away = away;
+    this.homePlayers = homePlayers;
+    this.awayPlayers = awayPlayers;
+    this.homeOrder = 0;
+    this.awayOrder = 0;
   }
 
-  public String getPitcher() {
-    return pitcher;
+  private Game(Player home, Player away) {
+    this.home = home;
+    this.away = away;
   }
 
-  public String getBatter() {
-    return batter;
+  public static Game of(List<Player> home, List<Player> away) {
+    return new Game(home.get(0), away.get(0), home, away);
   }
 
+  public void nextHome() {
+    this.home = homePlayers.get(++homeOrder);
+  }
+
+  public void nextAway() {
+    this.home = awayPlayers.get(++awayOrder);
+  }
+
+  public Player getHome() {
+    return home;
+  }
+
+  public Player getAway() {
+    return away;
+  }
+
+  public List<Player> getHomePlayers() {
+    return homePlayers;
+  }
+
+  public List<Player> getAwayPlayers() {
+    return awayPlayers;
+  }
 }
