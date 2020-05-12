70ce19b9ffd50fe16c503e728ed834e2ecff7403
Han <quaint3@gmail.com>
2020-05-12 17:54:47 +0900

    [#46] feature : GameResponse matchId 추가

--- BE/src/main/java/com/codesquad/baseball09/model/api/response/GameResponse.java
+++ BE/src/main/java/com/codesquad/baseball09/model/api/response/GameResponse.java
@@ -3,12 +3,18 @@ package com.codesquad.baseball09.model.api.response;
 public class GameResponse {
 
   private Long id;
+  private Long matchId;
 
-  public GameResponse(Long id) {
+  public GameResponse(Long id, Long matchId) {
     this.id = id;
+    this.matchId = matchId;
   }
 
   public Long getId() {
     return id;
+  }
+
+  public Long getMatchId() {
+    return matchId;
   }
 }
