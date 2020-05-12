4dbc66a07cf16279b16c0aa03f546f008d6f5000
sangwon21 <junothyme@gmail.com>
2020-05-12 21:27:55 +0900

    [#54] feat: custom hook 도입 - useFetch
    why: 서버에서 데이터를 받아오는 custom hook을 작성했다. 이는, 프로젝트
    내에서 여러 군데서 활용하기 위함이다.

--- FE/src/components/MainPage/GameSelectMenu/GameSelectMenu.tsx
+++ FE/src/components/MainPage/GameSelectMenu/GameSelectMenu.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import GameDispaly from "./GameDisplay/GameDisplay";
 import { IGame, gameList } from "@Model/gameList";
+import useFetch from "@/customHooks/useFetch";
 
 const Wrapper = styled.div`
   height: 400px;
@@ -22,8 +23,28 @@ const AvailInfoText = styled.div`
   }
 `;
 
+const DIV = styled.div`
+  font-size: 5rem;
+  color: #000;
+`;
+
+interface IServerGame {
+  id: number;
+  homeId: number;
+  awayId: number;
+  home: string;
+  away: string;
+  started: boolean;
+}
+
 const GameSelectMenu = () => {
   const [isGameAvailable, setIsGameAvailable] = useState<boolean>(true);
+  const { response, isLoading } = useFetch(`${process.env.API_ADDRESS}/main`, {
+    method: "GET",
+  });
+  if (isLoading) {
+    return <DIV>로딩 중이다...</DIV>;
+  }
 
   return (
     <>
@@ -33,9 +54,13 @@ const GameSelectMenu = () => {
           : "이미 선택된 팀입니다. 다른 팀을 선택해주세요!"}
       </AvailInfoText>
       <Wrapper>
-        {gameList &&
-          gameList.map((game: IGame) => {
-            const { matchName, homeTeam, awayTeam, isAvailable } = game;
+        {response &&
+          response.map((game: IServerGame) => {
+            const matchName = game.homeId.toString();
+            const homeTeam = game.home;
+            const awayTeam = game.away;
+            const isAvailable = game.started;
+
             return (
               <GameDispaly
                 matchName={matchName}
--- FE/src/components/MainPage/MainPage.tsx
+++ FE/src/components/MainPage/MainPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import GameSelectMenu from "@MainPage/GameSelectMenu/GameSelectMenu";
 import BenchClearing from "./bench-clearing.jpg";
