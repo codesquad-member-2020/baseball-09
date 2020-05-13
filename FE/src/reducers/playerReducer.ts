bc5bcca4c2c05b25765616b1093208cbbdd2382d
sohyun4283 <sohyun4283@naver.com>
2020-05-13 21:44:05 +0900

    [#65] feat: player 상태 변화 관리 함수 작성
    - 일단 어떤 액션이어도 1이 추가되도록 작성함

--- FE/src/reducers/playerReducer.ts
+++ FE/src/reducers/playerReducer.ts
@@ -1,23 +1,39 @@
 import { Action } from "@/actions/playerAction";
 import { PlayersState } from "@/contexts/playerContext";
 
+type Method = "atBat" | "outs" | "hits";
+const changePlayerStatus = (state: PlayersState, payloadId: number, method: Method) => {
+  const targetPlayer = state.players.filter((player) => player.id === payloadId)[0];
+  const players = [...state.players];
+  players.splice(players.indexOf(targetPlayer), 1, {
+    ...targetPlayer,
+    [method]: targetPlayer[method] + 1,
+  });
+
+  return players;
+};
+
 const playersReducer = (state: PlayersState, action: Action): PlayersState => {
   switch (action.type) {
     case "HIT":
       return {
         ...state,
+        players: changePlayerStatus(state, action.payload.id, "atBat"),
       };
     case "OUT":
       return {
         ...state,
+        players: changePlayerStatus(state, action.payload.id, "outs"),
       };
     case "BALL":
       return {
         ...state,
+        players: changePlayerStatus(state, action.payload.id, "atBat"),
       };
     case "STRIKE":
       return {
         ...state,
+        players: changePlayerStatus(state, action.payload.id, "hits"),
       };
     default:
       throw new Error("Unhandled action");
