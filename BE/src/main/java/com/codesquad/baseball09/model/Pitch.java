8293efa4cb934b685322b40d0fe2ef398750e5c8
Han <quaint3@gmail.com>
2020-05-14 00:02:54 +0900

    [#46] refactor : Enum 객체명 변경에 따른 수정
    주석 제거

--- BE/src/main/java/com/codesquad/baseball09/model/Pitch.java
+++ BE/src/main/java/com/codesquad/baseball09/model/Pitch.java
@@ -8,17 +8,13 @@ public class Pitch {
 
   private static final Logger logger = LoggerFactory.getLogger(Pitch.class);
 
-  public static State rollDice(int battingAverage) {
+  public static Status rollDice(int battingAverage) {
 
-//    logger.debug("hitProbability : {} ", battingAverage);
     double hitProbability = (double) battingAverage / 1000;
     double strikeBallProbability = (1 - hitProbability) / 2 - 0.05;
     double outProbability = 0.100;
 
-//    logger.debug("hitProbability : {} ", hitProbability);
-//    logger.debug("strikeBallProbability : {} ", strikeBallProbability);
-
-    State state = null;
+    Status status = null;
     boolean isGet = false;
     Random random = new Random();
 
@@ -26,41 +22,39 @@ public class Pitch {
 
       // 25% 확률 1~4
       int number = random.nextInt(4) + 1;
-//      logger.debug("number : {} ", number);
       double dice = Double.parseDouble(String.format("%.3f", random.nextDouble()));
-//      logger.debug("dice : {} ", dice);
 
       switch (number) {
 
         case 1:
           if (dice <= strikeBallProbability) {
-            state = State.STRIKE;
+            status = Status.STRIKE;
             isGet = true;
           }
           break;
 
         case 2:
           if (dice <= strikeBallProbability) {
-            state = State.BALL;
+            status = Status.BALL;
             isGet = true;
           }
           break;
 
         case 3:
           if (dice <= outProbability) {
-            state = State.OUT;
+            status = Status.OUT;
             isGet = true;
           }
           break;
 
         case 4:
           if (dice <= hitProbability) {
-            state = State.HIT;
+            status = Status.HIT;
             isGet = true;
           }
           break;
       }
     }
-    return state;
+    return status;
   }
 }
