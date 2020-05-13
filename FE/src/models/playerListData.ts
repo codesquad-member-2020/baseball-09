dec73d6fcb249730aeefba02d0efd62732f59c16
sohyun4283 <sohyun4283@naver.com>
2020-05-13 21:56:50 +0900

    [#64] feat: reducer 테스트를 위해 데이터 형태 변경

--- FE/src/models/playerListData.ts
+++ FE/src/models/playerListData.ts
@@ -1,122 +1,70 @@
-const playerListData = [
-  {
-    teamName: "Captin",
-    players: [
-      {
-        name: "김광진",
-        atBat: 1,
-        hits: 1,
-        outs: 0,
-      },
-      {
-        name: "이동규",
-        atBat: 1,
-        hits: 0,
-        outs: 1,
-      },
-      {
-        name: "김진수",
-        atBat: 1,
-        hits: 0,
-        outs: 1,
-      },
-      {
-        name: "박영권",
-        atBat: 1,
-        hits: 1,
-        outs: 0,
-      },
-      {
-        name: "추신수",
-        atBat: 1,
-        hits: 1,
-        outs: 0,
-      },
-      {
-        name: "이용대",
-        atBat: 1,
-        hits: 0,
-        outs: 1,
-      },
-      {
-        name: "류현진",
-        atBat: 1,
-        hits: 0,
-        outs: 1,
-      },
-      {
-        name: "최동수",
-        atBat: 1,
-        hits: 0,
-        outs: 1,
-      },
-      {
-        name: "한양범",
-        atBat: 1,
-        hits: 1,
-        outs: 0,
-      },
-    ],
-  },
-  {
-    teamName: "Marvel",
-    players: [
-      {
-        name: "김광진",
-        atBat: 1,
-        hits: 1,
-        outs: 0,
-      },
-      {
-        name: "이동규",
-        atBat: 1,
-        hits: 0,
-        outs: 1,
-      },
-      {
-        name: "김진수",
-        atBat: 1,
-        hits: 0,
-        outs: 1,
-      },
-      {
-        name: "박영권",
-        atBat: 1,
-        hits: 1,
-        outs: 0,
-      },
-      {
-        name: "추신수",
-        atBat: 1,
-        hits: 1,
-        outs: 0,
-      },
-      {
-        name: "이용대",
-        atBat: 1,
-        hits: 0,
-        outs: 1,
-      },
-      {
-        name: "류현진",
-        atBat: 1,
-        hits: 0,
-        outs: 1,
-      },
-      {
-        name: "최동수",
-        atBat: 1,
-        hits: 0,
-        outs: 1,
-      },
-      {
-        name: "한양범",
-        atBat: 1,
-        hits: 1,
-        outs: 0,
-      },
-    ],
-  },
-];
+const playerListData = {
+  teamName: "Captin",
+  players: [
+    {
+      id: 1,
+      name: "김광진",
+      atBat: 1,
+      hits: 1,
+      outs: 0,
+    },
+    {
+      id: 2,
+      name: "이동규",
+      atBat: 1,
+      hits: 0,
+      outs: 1,
+    },
+    {
+      id: 3,
+      name: "김진수",
+      atBat: 1,
+      hits: 0,
+      outs: 1,
+    },
+    {
+      id: 4,
+      name: "박영권",
+      atBat: 1,
+      hits: 1,
+      outs: 0,
+    },
+    {
+      id: 5,
+      name: "추신수",
+      atBat: 1,
+      hits: 1,
+      outs: 0,
+    },
+    {
+      id: 6,
+      name: "이용대",
+      atBat: 1,
+      hits: 0,
+      outs: 1,
+    },
+    {
+      id: 7,
+      name: "류현진",
+      atBat: 1,
+      hits: 0,
+      outs: 1,
+    },
+    {
+      id: 8,
+      name: "최동수",
+      atBat: 1,
+      hits: 0,
+      outs: 1,
+    },
+    {
+      id: 9,
+      name: "한양범",
+      atBat: 1,
+      hits: 1,
+      outs: 0,
+    },
+  ],
+};
 
 export default playerListData;
