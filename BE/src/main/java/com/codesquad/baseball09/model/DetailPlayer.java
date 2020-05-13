683a1618d731b0c0452e4530687fb251cabd8f09
Han <quaint3@gmail.com>
2020-05-14 00:03:50 +0900

    [#46] feature : 변수 추가, 빌더 추가
    상세 플레이어 페이지를 보여줄 클래스 변수 추가
    변수갯수의 추가로, 빌더 생성

--- BE/src/main/java/com/codesquad/baseball09/model/DetailPlayer.java
+++ BE/src/main/java/com/codesquad/baseball09/model/DetailPlayer.java
@@ -2,16 +2,35 @@ package com.codesquad.baseball09.model;
 
 public class DetailPlayer {
 
-  private final String name;
-  private final int atBat;
-  private final int hits;
-  private final int outs;
+  private Long gameId;
+  private Long teamId;
+  private Long playerId;
+  private String name;
+  private int atBat;
+  private int hits;
+  private int outs;
 
-  public DetailPlayer(String name, int atBat, int hits, int outs) {
+  private DetailPlayer(Long gameId, Long teamId, Long playerId, String name, int atBat, int hits,
+      int outs) {
+    this.gameId = gameId;
+    this.teamId = teamId;
+    this.playerId = playerId;
     this.name = name;
     this.atBat = atBat;
     this.hits = hits;
     this.outs = outs;
+  }
+
+  public Long getGameId() {
+    return gameId;
+  }
+
+  public Long getTeamId() {
+    return teamId;
+  }
+
+  public Long getPlayerId() {
+    return playerId;
   }
 
   public String getName() {
@@ -28,5 +47,62 @@ public class DetailPlayer {
 
   public int getOuts() {
     return outs;
+  }
+
+  public static final class Builder {
+
+    private Long gameId;
+    private Long teamId;
+    private Long playerId;
+    private String name;
+    private int atBat;
+    private int hits;
+    private int outs;
+
+    private Builder() {
+    }
+
+    public static Builder of() {
+      return new Builder();
+    }
+
+    public Builder gameId(Long gameId) {
+      this.gameId = gameId;
+      return this;
+    }
+
+    public Builder teamId(Long teamId) {
+      this.teamId = teamId;
+      return this;
+    }
+
+    public Builder playerId(Long playerId) {
+      this.playerId = playerId;
+      return this;
+    }
+
+    public Builder name(String name) {
+      this.name = name;
+      return this;
+    }
+
+    public Builder atBat(int atBat) {
+      this.atBat = atBat;
+      return this;
+    }
+
+    public Builder hits(int hits) {
+      this.hits = hits;
+      return this;
+    }
+
+    public Builder outs(int outs) {
+      this.outs = outs;
+      return this;
+    }
+
+    public DetailPlayer build() {
+      return new DetailPlayer(gameId, teamId, playerId, name, atBat, hits, outs);
+    }
   }
 }
