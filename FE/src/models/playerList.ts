cc0b5ded9c2cdbe786b6b3166f30b41f9f4b3d52
sohyun4283 <sohyun4283@naver.com>
2020-05-13 22:02:25 +0900

    [#64] feat: 선수 명단 리스트 생성 부분 삭제
    - 필요하지 않다고 판단해 삭제함

--- FE/src/models/playerList.ts
+++ FE/src/models/playerList.ts
@@ -1,24 +1,14 @@
-import playerListData from "./playerListData";
-
 interface IPlayerList {
   teamName: string;
   players: IPlayer[];
 }
 
 interface IPlayer {
+  id: number;
   name: string;
   atBat: number;
   hits: number;
   outs: number;
 }
 
-const getPlayerList = (playerListInfo: IPlayerList) => {
-  return playerListInfo;
-};
-
-const playerList: IPlayerList[] = [];
-playerListData.forEach((player: IPlayerList, index: number) => {
-  playerList[index] = getPlayerList(player);
-});
-
-export { IPlayerList, IPlayer, playerList };
+export { IPlayerList, IPlayer };
