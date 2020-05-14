6569eddd281c7ea375b44a6c280d67cb2b928ac1
Han <quaint3@gmail.com>
2020-05-14 15:37:38 +0900

    [#69] refactor : 공 던지는 요청시 몇가지 변수 추가
    비즈니스 로직을 원활하게 진행하기 위해서
    request에 몇가지 내부 변수를 추가했음.

--- BE/src/main/java/com/codesquad/baseball09/model/api/request/PitchRequest.java
+++ BE/src/main/java/com/codesquad/baseball09/model/api/request/PitchRequest.java
@@ -3,12 +3,17 @@ package com.codesquad.baseball09.model.api.request;
 public class PitchRequest {
 
   private Long gameId;
-  private int inning;
+  private Long teamId;
   private Long playerId;
+  private int inning;
   private int battingAverage;
 
   public Long getGameId() {
     return gameId;
+  }
+
+  public Long getTeamId() {
+    return teamId;
   }
 
   public Long getPlayerId() {
