276b96a249df1b05ed276e0bd832a32163e838ee
Han <quaint3@gmail.com>
2020-05-14 15:39:11 +0900

    [#69] feature : Board, Game 에 필요한 변수 추가
    객체별로 겹치는 부분이 많음.
    재설계 및 리팩토링이 절실히 필요하다.

--- BE/src/main/java/com/codesquad/baseball09/model/Board.java
+++ BE/src/main/java/com/codesquad/baseball09/model/Board.java
@@ -1,34 +1,61 @@
 package com.codesquad.baseball09.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.List;
+
 public class Board {
 
   private Long gameId;
   private int inning;
 
+  @JsonIgnore
+  private Long homeId;
   private String homeName;
   private int homeScore;
+  @JsonIgnore
+  private int homeOrder;
 
+  @JsonIgnore
+  private Long awayId;
   private String awayName;
   private int awayScore;
+  @JsonIgnore
+  private int awayOrder;
+
   private boolean isBottom;
 
   private Game game;
   private InningStatus status;
-  private BattingLog log;
+  private List<BattingLog> log;
 
-  public Board(Long gameId, int inning, String homeName, int homeScore, String awayName,
-      int awayScore, boolean isBottom, Game game, InningStatus status,
-      BattingLog log) {
+  private Board(Long gameId, int inning, Long homeId, String homeName, int homeScore, int homeOrder,
+      Long awayId, String awayName, int awayScore, int awayOrder, boolean isBottom, Game game,
+      InningStatus status,
+      List<BattingLog> log) {
     this.gameId = gameId;
     this.inning = inning;
+    this.homeId = homeId;
     this.homeName = homeName;
     this.homeScore = homeScore;
+    this.homeOrder = homeOrder;
+    this.awayId = awayId;
     this.awayName = awayName;
     this.awayScore = awayScore;
+    this.awayOrder = awayOrder;
     this.isBottom = isBottom;
     this.game = game;
     this.status = status;
     this.log = log;
+  }
+
+  public void change() {
+    this.inning++;
+
+    if (isBottom) {
+      isBottom = false;
+    } else {
+      isBottom = true;
+    }
   }
 
   public Long getGameId() {
@@ -39,6 +66,10 @@ public class Board {
     return inning;
   }
 
+  public Long getHomeId() {
+    return homeId;
+  }
+
   public String getHomeName() {
     return homeName;
   }
@@ -47,12 +78,24 @@ public class Board {
     return homeScore;
   }
 
+  public int getHomeOrder() {
+    return homeOrder;
+  }
+
+  public Long getAwayId() {
+    return awayId;
+  }
+
   public String getAwayName() {
     return awayName;
   }
 
   public int getAwayScore() {
     return awayScore;
+  }
+
+  public int getAwayOrder() {
+    return awayOrder;
   }
 
   public boolean isBottom() {
@@ -67,7 +110,7 @@ public class Board {
     return status;
   }
 
-  public BattingLog getLog() {
+  public List<BattingLog> getLog() {
     return log;
   }
 
@@ -77,19 +120,40 @@ public class Board {
     private Long gameId;
     private int inning;
     private String homeName;
+    private Long homeId;
     private int homeScore;
+    private int homeOrder;
+    private Long awayId;
     private String awayName;
     private int awayScore;
+    private int awayOrder;
     private boolean isBottom;
     private Game game;
     private InningStatus status;
-    private BattingLog log;
+    private List<BattingLog> log;
 
     private Builder() {
     }
 
-    public static Builder aBoard() {
+    public static Builder of() {
       return new Builder();
+    }
+
+    public Builder(Board board) {
+      this.gameId = board.getGameId();
+      this.inning = board.getInning();
+      this.homeId = board.getHomeId();
+      this.homeName = board.getHomeName();
+      this.homeScore = board.getHomeScore();
+      this.homeOrder = board.getHomeOrder();
+      this.awayId = board.getAwayId();
+      this.awayName = board.getAwayName();
+      this.awayScore = board.getAwayScore();
+      this.awayOrder = board.getAwayOrder();
+      this.isBottom = board.isBottom();
+      this.game = board.getGame();
+      this.status = board.getStatus();
+      this.log = board.getLog();
     }
 
     public Builder gameId(Long gameId) {
@@ -99,6 +163,11 @@ public class Board {
 
     public Builder inning(int inning) {
       this.inning = inning;
+      return this;
+    }
+
+    public Builder homeId(Long homeId) {
+      this.homeId = homeId;
       return this;
     }
 
@@ -112,6 +181,16 @@ public class Board {
       return this;
     }
 
+    public Builder homeOrder(int homeOrder) {
+      this.homeOrder = homeOrder;
+      return this;
+    }
+
+    public Builder awayId(Long awayId) {
+      this.awayId = awayId;
+      return this;
+    }
+
     public Builder awayName(String awayName) {
       this.awayName = awayName;
       return this;
@@ -122,6 +201,11 @@ public class Board {
       return this;
     }
 
+    public Builder awayOrder(int awayOrder) {
+      this.awayOrder = awayOrder;
+      return this;
+    }
+
     public Builder isBottom(boolean isBottom) {
       this.isBottom = isBottom;
       return this;
@@ -129,6 +213,8 @@ public class Board {
 
     public Builder game(Game game) {
       this.game = game;
+      this.homeId(game.getHomeTeamId());
+      this.awayId(game.getAwayTeamId());
       return this;
     }
 
@@ -137,14 +223,29 @@ public class Board {
       return this;
     }
 
-    public Builder log(BattingLog log) {
+    public Builder score(List<Score> scores) {
+      for (int i = 0; i < scores.size(); i++) {
+        Score score = scores.get(i);
+        if (score.getTeamId().equals(homeId)) {
+          homeName(score.getName());
+          homeScore(score.getScore());
+        }
+        if (score.getTeamId().equals(awayId)) {
+          awayName(score.getName());
+          awayScore(score.getScore());
+        }
+      }
+      return this;
+    }
+
+    public Builder log(List<BattingLog> log) {
       this.log = log;
       return this;
     }
 
     public Board build() {
-      return new Board(gameId, inning, homeName, homeScore, awayName, awayScore, isBottom, game,
-          status, log);
+      return new Board(gameId, inning, homeId, homeName, homeScore, homeOrder, awayId, awayName,
+          awayScore, awayOrder, isBottom, game, status, log);
     }
   }
 }
--- BE/src/main/java/com/codesquad/baseball09/model/Game.java
+++ BE/src/main/java/com/codesquad/baseball09/model/Game.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Game {
 
+  private Long homeTeamId;
+
+  private Long awayTeamId;
+
   private Player home;
 
   private Player away;
@@ -19,18 +23,22 @@ public class Game {
   @JsonIgnore
   private int awayOrder;
 
-  private Game(Player home, Player away,
-      List<Player> homePlayers, List<Player> awayPlayers) {
+  private Game(Long homeTeamId, Long awayTeamId, Player home, Player away,
+      List<Player> homePlayers, List<Player> awayPlayers, int homeOrder, int awayOrder) {
+    this.homeTeamId = homeTeamId;
+    this.awayTeamId = awayTeamId;
     this.home = home;
     this.away = away;
     this.homePlayers = homePlayers;
     this.awayPlayers = awayPlayers;
-    this.homeOrder = 0;
-    this.awayOrder = 0;
+    this.homeOrder = homeOrder;
+    this.awayOrder = awayOrder;
   }
 
-  public static Game of(List<Player> home, List<Player> away) {
-    return new Game(home.get(0), away.get(0), home, away);
+  public static Game of(Long homeTeamId, Long awayTeamId, List<Player> home,
+      List<Player> away, int homeOrder, int awayOrder) {
+    return new Game(homeTeamId, awayTeamId, home.get(0), away.get(0), home, away, homeOrder,
+        awayOrder);
   }
 
   public void nextHome() {
@@ -49,6 +57,14 @@ public class Game {
     this.away = awayPlayers.get(++awayOrder);
   }
 
+  public Long getHomeTeamId() {
+    return homeTeamId;
+  }
+
+  public Long getAwayTeamId() {
+    return awayTeamId;
+  }
+
   public Player getHome() {
     return home;
   }
@@ -63,5 +79,13 @@ public class Game {
 
   public List<Player> getAwayPlayers() {
     return awayPlayers;
+  }
+
+  public int getHomeOrder() {
+    return homeOrder;
+  }
+
+  public int getAwayOrder() {
+    return awayOrder;
   }
 }
